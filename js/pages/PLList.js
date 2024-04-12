88c5e34a96176fa998a82e540c7ac3c778939d52
Tami5535 <159556247+Tami5535@users.noreply.github.com>
2024-04-12 17:03:26 +0500

    sdsdsdsa

--- js/pages/PLList.js
+++ js/pages/PLList.js
@@ -79,7 +79,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
-                                <p>{{ record.hz }}ГЦ</p>
+                                <p>{{ record.hz }}ФПС</p>
                             </td>
                         </tr>
                     </table>
